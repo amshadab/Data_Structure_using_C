27c8685605638719957e7205697448ee06838884
Shadab Ansari <139966782+amshadab@users.noreply.github.com>
2023-09-05 15:17:47 +0530

    Update Bubble.c

--- Bubble.c
+++ Bubble.c
@@ -14,7 +14,7 @@ void main()
     }
     for (i = 0; i < n; i++)
     {
-        for (j = 0; j < (n - 1) - i; j++)
+        for (j = 0; j < (n - i) - 1; j++)
         {
             if(arr[j]>arr[j+1])
             {

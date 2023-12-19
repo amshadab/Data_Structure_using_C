800efb7d0b18afdaf4cd59b9a22549c46674c55f
Shadab Ansari <139966782+amshadab@users.noreply.github.com>
2023-12-19 21:18:55 +0530

    Update Binsearch.c

--- Binsearch.c
+++ Binsearch.c
@@ -1,44 +1,50 @@
-//Binary search
-#include<stdio.h>
-#include<conio.h>
+// Binary search
+#include <stdio.h>
+#include <conio.h>
 void main()
 {
-    int low,high,mid,target,arr[50],n,i;
+    int low, high, mid, target, arr[50], n, i;
     printf("Enter the size of an array: ");
-    scanf("%d",&n);
-    printf("\nEnter element i an array");
-    for(i=0;i<n;i++)
+    scanf("%d", &n);
+    printf("\nEnter element in an array");
+    for (i = 0; i < n; i++)
     {
-        printf("\narr[%d]= ",i);
-        scanf("%d",&arr[i]);
+        printf("\narr[%d]= ", i);
+        scanf("%d", &arr[i]);
     }
 
     printf("Enter element to be search: ");
-    scanf("%d",&target);
-    low=0;
-    high=n;
-    mid=(low+high)/2;
-    if(arr[mid]==target)
+    scanf("%d", &target);
+    low = 0;
+    high = n;
+    mid = (low + high) / 2;
+    if (arr[mid] == target)
     {
-        printf("%d is found at %d",target,mid);
+        printf("%d is found at %d", target, mid);
     }
-    else{
-    for(i=0;i<n;i++)
+    else
     {
-    if(target>arr[mid])
-    {
-        low=mid+1;
-    } 
-    else{
-        high=mid-1;
-    }
-    mid=(high+low)/2;
-    if(arr[mid]==target)
-    {
-        printf("%d found at %d",target,mid);
-        break;
-    }
-    }
+        for (i = 0; i < n; i++)
+        {
+            if (target > arr[mid])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+            mid = (high + low) / 2;
+            if (arr[mid] == target)
+            {
+                printf("%d found at %d", target, mid);
+                break;
+            }
+        }
+        if (i == n)
+        {
+            printf("%d not found", target);
+        }
     }
     getch();
 }
